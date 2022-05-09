52ad8f5a08e57ebe8fe6c21dd1ddda19209f3447
Bruno Sotto-Mayor <machadob@post.bgu.ac.il>
2022-05-09 16:07:00 +0300

    changes setup

--- setup.py
+++ setup.py
@@ -8,8 +8,8 @@ setup(
     license='MIT',
     description='Brute-Force All of sklearn!',
     long_description=open('README.txt').read(),
-    install_requires=['numpy'],
-    url='https://github.com/ypeleg/HungaBunga',
+    install_requires=['numpy', 'scipy', 'joblib', 'scikit-learn', 'tabulate', 'tqdm'],
+    url='https://github.com/Bruno81930/HungaBunga',
     author='Yam Peleg',
     author_email='yam@deeptrading.com'
 )
